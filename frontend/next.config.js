5f9e519ba217c49040967ab5b644d6dec0f02d9c
Deymosik <stepanovvadim472@gmail.com>
2025-12-08 22:10:16 +0600

    Commit V1

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-
+    output: 'standalone',
     // Читаем URL бэкенда из переменных окружения, или фолбэк на локалхост
     env: {
         DJANGO_API_URL: process.env.DJANGO_API_URL || 'http://127.0.0.1:8000',
